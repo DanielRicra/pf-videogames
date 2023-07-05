ee3e9f67cb943ef0a8307d6b9c4e167422f3e210
DanielRicra <daniel.int@outlook.com>
2023-07-05 15:27:07 -0500

    Update App.js to add img background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,14 @@ import Search from './pages/Search'
 
 function App() {
   return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='/detail/:id' element={<Detail />} />
-      </Routes>
+    <div className={'bg-[url(./assets/bg-img.jpg)] h-screen w-screen overflow-hidden'}>
+      <div className='overflow-y-auto w-full h-full text-white'>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/search' element={<Search />} />
+          <Route path='/detail/:id' element={<Detail />} />
+        </Routes>
+      </div>
     </div>
   )
 }
