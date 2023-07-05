8d17a43bd55be35a555491e34e9e0c99861dd248
Francaios <donnarif@outlook.com>
2023-07-04 21:38:39 -0300

    Arregle getVideogamesById

--- api/src/controllers/getVideogamesById.js
+++ api/src/controllers/getVideogamesById.js
@@ -1,25 +1,11 @@
-const { API_KEY } = process.env;
+
 const { Videogame, Genre }= require('../db')
-const axios = require ('axios');
 
 const getVideogamesById = async ( searchedId ) =>{
     try{
-        let dbVideogames = await Videogame.findByPk(searchedId,{ include: Genre });
-        console.log(dbVideogames)
-        if (dbVideogames) return dbVideogames
-        let response = await axios(`https://api.rawg.io/api/games/${searchedId}?key=dabc73d2d7c347dbb54c066ae10d71c1`)
-        let videogame = response.data
-        const result ={
-            name:videogame.name,
-            id:videogame.id,
-            image:videogame.background_image,
-            description:videogame.description,
-            rating:videogame.rating,
-            releaseDate:videogame.released,
-            platform:videogame.platforms.map(plat => plat.platform.name),
-            GenreId:videogame.genres.map(gen => gen.id)
-        }
-        return result
+        let dbVideogame = await Videogame.findByPk(searchedId,{ include: Genre },{ include: Tag});
+        
+        return dbVideogame
     }
     catch (error) {
         return {error: error.message}
