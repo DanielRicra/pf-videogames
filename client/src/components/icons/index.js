017a69c443c23345c931aef9cf423bb3557aebf9
DanielRicra <daniel.int@outlook.com>
2023-07-05 20:51:33 -0500

    Add Heart, Search and User componen icons

--- client/src/components/icons/index.js
+++ client/src/components/icons/index.js
@@ -1,12 +1,17 @@
-import ChevronLeftIcon from './ChevronLeft';
-import ChevronRightIcon from './ChevronRight';
-import ShoppingCartIcon from './ShoppingCart';
-import CloudUpload from './CloudUpload';
-
+import ChevronLeftIcon from './ChevronLeft'
+import ChevronRightIcon from './ChevronRight'
+import ShoppingCartIcon from './ShoppingCart'
+import CloudUpload from './CloudUpload'
+import SearchIcon from './Search'
+import HeartIcon from './Heart'
+import UserIcon from './User'
 
 export {
-   ChevronLeftIcon,
-   ChevronRightIcon,
-   ShoppingCartIcon,
-   CloudUpload
+  ChevronLeftIcon,
+  ChevronRightIcon,
+  ShoppingCartIcon,
+  CloudUpload,
+  SearchIcon,
+  HeartIcon,
+  UserIcon,
 }
