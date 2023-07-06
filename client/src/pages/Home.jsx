58cc6f04fd31889221ba6f5e02a0bac2ef2836aa
DanielRicra <daniel.int@outlook.com>
2023-07-05 21:16:05 -0500

    Fix styles in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,27 @@
-import { useSelector } from 'react-redux';
-import Carrousel from '../components/Carrousel';
-import { videogames } from '../utils/dumbData';
+import { useSelector } from 'react-redux'
+import Carrousel from '../components/Carrousel'
+import { videogames } from '../utils/dumbData'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
-  const { user } = useSelector((state) => state.users);
-  console.log('🚀 ~ file: Home.jsx:7 ~ Home ~ user:', user);
+  const { user } = useSelector((state) => state.users)
+
   return (
-    <div className='text-white min-h-screen'>
+    <div className='text-white min-h-screen py-14'>
       <div className='font-medium mb-5 text-7xl whitespace-normal ml-12'>
-        <h1>Welcome to <span className='text-purple-400'>the best</span> <br/> PC videogames <br/> E-comerce</h1>
+        <h1 className='my-10 ml-2 lg:ml-10'>
+          Welcome to <span className='text-purple-400'>the best</span> <br /> PC
+          videogames <br /> E-commerce
+        </h1>
       </div>
 
       <br />
 
-      <div className='font-medium mb-5 text-4xl ml-12'>
-        <h3>Popular Games</h3>
+      <div className='mb-5 mx-12 flex justify-between items-center'>
+        <h3 className='ml-2 text-4xl lg:ml-10 font-normal'>Popular Games</h3>
+        <Link to='/search' className='text-base'>
+          See all the games &#8594;
+        </Link>
       </div>
 
       <Carrousel cards={videogames} />
@@ -25,5 +32,5 @@ const Home = () => {
       </div>
     </div>
   )
-};
-export default Home;
+}
+export default Home
