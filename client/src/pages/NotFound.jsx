4c38cebd05e7f2bf62573414b84ca3b61e9b999b
LiamOtegui <liamotegui@gmail.com>
2023-07-06 14:50:37 -0300

    Update NotFound ... #21 update assets new image

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,4 +1,22 @@
+import crash from "../assets/crash.png"
+import { NavLink } from "react-router-dom"
+
 const NotFound = () => {
-  return <div>NotFound</div>
+  return (
+    <>
+      <div class="wrapper flex items-center justify-center my-[7rem]">
+        <div class="text-[3rem] font-mono font-semibold mx-[5rem]">
+          Oops! this page doesn't exist.
+        </div>
+        <img src={crash} class="h-[15rem]" />
+      </div>
+
+      <div class="flex w-[8rem] text-[1.3rem] font-semibold justify-center bg-blue-600 border-[0.2rem] border-blue-600 rounded-[0.5rem] mx-[44rem]">
+        <NavLink to="/">
+          Main menu
+        </NavLink>
+      </div>
+    </>
+  )
 }
 export default NotFound
