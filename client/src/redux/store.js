26e35b3f3843f0cc09f7e832b2b7f0e139c8571f
DanielRicra <daniel.int@outlook.com>
2023-07-06 11:41:52 -0500

    update store.js to create the store
    Add reducer of cart, user and videogames

---
+++ client/src/redux/store.js
@@ -0,0 +1,14 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice'
+import cartReducer from './cart/cartSlice'
+import videoGameReducer from './videogame/videoGameSlice'
+
+const store = configureStore({
+  reducer: {
+    users: userReducer,
+    cart: cartReducer,
+    videoGame: videoGameReducer,
+  },
+})
+
+export default store
