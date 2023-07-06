a8d35bf44dadd86f2edfc3e4d14d4252f850676a
DanielRicra <daniel.int@outlook.com>
2023-07-06 11:31:35 -0500

    Add redux folder with all slices, and some actions

---
+++ client/src/redux/actions/videoGamesAction.js
@@ -0,0 +1,12 @@
+import { setLoading } from '../videogame/videoGameSlice'
+
+export const createVideoGame = () => {
+  return async (dispatch) => {
+    dispatch(setLoading(true))
+    try {
+      /* empty */
+    } catch (error) {
+      /* empty */
+    }
+  }
+}
