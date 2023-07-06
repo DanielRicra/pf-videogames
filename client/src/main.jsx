dcaa556c94ab594eaebb56f4a8891aaa43070a8a
DanielRicra <daniel.int@outlook.com>
2023-07-06 11:40:45 -0500

    Fix and reorder imports

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom'
+
+import App from './App.jsx'
+import store from './redux/store'
 import './index.css'
-import store from './redux/store.js'
 
 const root = document.getElementById('root')
 
