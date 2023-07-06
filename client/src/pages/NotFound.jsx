22a38bbfee4d2ef547ff8bf0506d4159c7794992
LiamOtegui <liamotegui@gmail.com>
2023-07-06 18:36:42 -0300

    Cambio de class a className en Detail page y en NotFound component ... Update del Footer

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -4,14 +4,14 @@ import { NavLink } from "react-router-dom"
 const NotFound = () => {
   return (
     <>
-      <div class="wrapper flex items-center justify-center my-[7rem]">
-        <div class="text-[3rem] font-mono font-semibold mx-[5rem]">
+      <div className="wrapper flex items-center justify-center my-[7rem]">
+        <div className="text-[3rem] font-mono font-semibold mx-[5rem]">
           Oops! this page doesn't exist.
         </div>
-        <img src={crash} class="h-[15rem]" />
+        <img src={crash} className="h-[15rem]" />
       </div>
 
-      <div class="flex w-[8rem] text-[1.3rem] font-semibold justify-center bg-blue-600 border-[0.2rem] border-blue-600 rounded-[0.5rem] mx-[44rem]">
+      <div className="flex w-[8rem] text-[1.3rem] font-semibold justify-center bg-blue-600 border-[0.2rem] border-blue-600 rounded-[0.5rem] mx-[44rem]">
         <NavLink to="/">
           Main menu
         </NavLink>
