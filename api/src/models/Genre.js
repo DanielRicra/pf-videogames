a44d62ae03109251f4435401f68b97b1b3a3ed80
Francaios <donnarif@outlook.com>
2023-07-07 15:25:50 -0300

    Añadi la funcion que carga las imagenes al bucket, el filtro para que no muestre juegos sin stock, y añadi stock y price aleatorios a los videogames

--- api/src/models/Genre.js
+++ api/src/models/Genre.js
@@ -8,7 +8,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     id: {
-        type: DataTypes.INTEGER,
+        type: DataTypes.STRING,
         primaryKey: true,
         allowNull: false,
     }
