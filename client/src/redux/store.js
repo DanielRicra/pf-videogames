c11dbeafec693618587d897f1248c62f6cc7d2e5
DanielRicra <daniel.int@outlook.com>
2023-07-08 23:38:51 -0500

    Add the redux fodler with all slices and actions, cause was deleted

---
+++ client/src/redux/store.js
@@ -0,0 +1,16 @@
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './user/userSlice'
+import cartReducer from './cart/cartSlice'
+import videoGameReducer from './videogame/videoGameSlice'
+import reviewReducer from './Reducers/reviewReducer';
+
+const store = configureStore({
+  reducer: {
+    users: userReducer,
+    cart: cartReducer,
+    review: reviewReducer,
+    videoGames: videoGameReducer,
+  },
+})
+
+export default store
