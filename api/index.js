dfb7ae34ebc9a419462738487056a0e67dd0e71c
DanielRicra <daniel.int@outlook.com>
2023-07-09 11:44:44 -0500

    Add the import of the models to use in the uploadGenres, ..etc.
    - Por ahora estan comentados los uploadsGenres, uploadsTags and uploadsVideogames, DESCOMENTAR para utilizarlo, solo para actualizar los videojuegos con el nuevo url de imgs

--- api/index.js
+++ api/index.js
@@ -1,10 +1,10 @@
 const server = require('./src/app.js')
-const { conn } = require('./src/db.js')
-// const {
-//   uploadVideogames,
-//   uploadTags,
-//   uploadGenres,
-// } = require('./src/utils/helpers.js')
+const { conn, Tag, Videogame, Genre } = require('./src/db.js')
+const {
+  uploadVideogames,
+  uploadTags,
+  uploadGenres,
+} = require('./src/utils/helpers.js')
 
 const PORT = 3001
 
