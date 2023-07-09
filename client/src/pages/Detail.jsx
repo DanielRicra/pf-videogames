b4dfbb6da43ef414c6bb49e6ab4268e2861ecc3b
DanielRicra <daniel.int@outlook.com>
2023-07-08 23:47:50 -0500

    Fix the import of reviewActions
    format document
    
    Remove commented import of navbar

--- client/src/pages/Detail.jsx
+++ client/src/pages/Detail.jsx
@@ -1,35 +1,29 @@
-import { useDispatch, useSelector } from "react-redux";
-import { useParams, useNavigate } from "react-router-dom";
-import { useEffect } from "react";
-import { IconHeart } from '@tabler/icons-react';
-import { IconShoppingCartPlus } from '@tabler/icons-react';
-import Footer from '../components/Footer';
-import { ReviewCard, ReviewForm } from "../components";
-import { fetchReviews, postReview } from "../Redux/actions/reviewAction";
-
-
-// import del componente NavBar
-
+import { useDispatch, useSelector } from 'react-redux'
+import { useParams, useNavigate } from 'react-router-dom'
+import { useEffect } from 'react'
+import { IconHeart } from '@tabler/icons-react'
+import { IconShoppingCartPlus } from '@tabler/icons-react'
+import Footer from '../components/Footer'
+import { ReviewCard, ReviewForm } from '../components'
+import { fetchReviews, postReview } from '../redux/actions/reviewAction'
 // import de las actions getDetail, clearDetail
 
 const Detail = () => {
-  const { id } = useParams();
-  const reviews = useSelector(state => state.review.reviews || []);
-  const dispatch = useDispatch();
-  const navigate = useNavigate();
+  const { id } = useParams()
+  const reviews = useSelector((state) => state.review.reviews || [])
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
 
   useEffect(() => {
-    dispatch(fetchReviews(id));
-  }, [dispatch, id]);
+    dispatch(fetchReviews(id))
+  }, [dispatch, id])
 
   const handleSubmitReview = (review) => {
-    dispatch(postReview(review));
-  };
-
+    dispatch(postReview(review))
+  }
 
   // const [currentId, setCurrentId] = useState(id);
   // const [loading, setLoading] = useState(true); // para el componente Loading si es que vamos a usar.
-
 
   //  useEffect(() => {
   //   setLoading(true);
@@ -40,7 +34,6 @@ const Detail = () => {
   //     dispatch(clearDetail());
   //   };
   //  }, [dispatch, currentId]);
-
 
   // if (loading) {
   //    return <Loading />;
@@ -62,46 +55,65 @@ const Detail = () => {
           <div/>
 
       </div> */}
-      <div className="wrapper">
-        <div className="flex">
-          <div className="container1">
-            <img className="mx-[5rem] my-[5rem]" src="https://cdn.akamai.steamstatic.com/steam/apps/1593500/capsule_616x353.jpg?t=1650554420" alt="{videogame.name}" title="{videogame.name}" />
+      <div className='wrapper'>
+        <div className='flex'>
+          <div className='container1'>
+            <img
+              className='mx-[5rem] my-[5rem]'
+              src='https://cdn.akamai.steamstatic.com/steam/apps/1593500/capsule_616x353.jpg?t=1650554420'
+              alt='{videogame.name}'
+              title='{videogame.name}'
+            />
 
-            <div className="flex justify-between px-[21rem]">
-              <button className="h-[4rem] mx-[-15.5rem] my-[-4rem]" onClick={() => navigate(`/favorites`)}> <IconHeart className="w-full h-full" /> </button>
-              <button className="h-[4rem] mx-[7rem] my-[-4rem]" onClick={() => navigate(`/cart`)}> <IconShoppingCartPlus className="w-full h-full" /> </button>
+            <div className='flex justify-between px-[21rem]'>
+              <button
+                className='h-[4rem] mx-[-15.5rem] my-[-4rem]'
+                onClick={() => navigate('/favorites')}
+              >
+                {' '}
+                <IconHeart className='w-full h-full' />{' '}
+              </button>
+              <button
+                className='h-[4rem] mx-[7rem] my-[-4rem]'
+                onClick={() => navigate('/cart')}
+              >
+                {' '}
+                <IconShoppingCartPlus className='w-full h-full' />{' '}
+              </button>
             </div>
           </div>
 
-          <div className="container2 bg-violet-900 p-10 w-[40rem] border-4 border-violet-700 rounded-2xl mx-[5rem] my-[5rem]">
-            <div className="text-6xl font-serif text-white text-center">
+          <div className='container2 bg-violet-900 p-10 w-[40rem] border-4 border-violet-700 rounded-2xl mx-[5rem] my-[5rem]'>
+            <div className='text-6xl font-serif text-white text-center'>
               Name
             </div>
-            <div className="text-1xl font-serif text-white mx-[2rem] my-[2rem]">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem possimus reprehenderit recusandae, incidunt delectus quasi, ipsum reiciendis quaerat optio aspernatur commodi harum odit? Culpa quas officiis repellendus. Tempora, porro officiis.
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex voluptate in quam dolorem sed qui reprehenderit praesentium amet saepe aperiam, sunt quaerat ut, rerum quisquam nulla atque! In, vel soluta.
+            <div className='text-1xl font-serif text-white mx-[2rem] my-[2rem]'>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem
+              possimus reprehenderit recusandae, incidunt delectus quasi, ipsum
+              reiciendis quaerat optio aspernatur commodi harum odit? Culpa quas
+              officiis repellendus. Tempora, porro officiis. Lorem ipsum dolor
+              sit amet consectetur adipisicing elit. Ex voluptate in quam
+              dolorem sed qui reprehenderit praesentium amet saepe aperiam, sunt
+              quaerat ut, rerum quisquam nulla atque! In, vel soluta.
             </div>
 
-            <div className="container flex">
-              <div className="text-2xl font-serif text-white mx-[3rem]">
+            <div className='container flex'>
+              <div className='text-2xl font-serif text-white mx-[3rem]'>
                 Genres:
               </div>
-              <div className="text-2xl font-serif text-white mx-[13rem]">
+              <div className='text-2xl font-serif text-white mx-[13rem]'>
                 Released:
               </div>
             </div>
           </div>
-
         </div>
 
-        <div className="text-3xl text-yellow-500 mx-[77rem] my-[-3rem]">
-
-        </div>
+        <div className='text-3xl text-yellow-500 mx-[77rem] my-[-3rem]'></div>
 
         {/*  <ReviewForm onSubmit={handleSubmitReview} videogameId={id} /> */}
 
-        <div className="flex flex-col mt-10">
-          {reviews.map(review => (
+        <div className='flex flex-col mt-10'>
+          {reviews.map((review) => (
             <ReviewCard key={review.id} review={review} />
           ))}
         </div>
@@ -111,4 +123,4 @@ const Detail = () => {
   )
 }
 
-export default Detail;
+export default Detail
