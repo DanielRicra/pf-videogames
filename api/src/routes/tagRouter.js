542c131d048df70663b05b43cb002fbfe1c28608
DanielRicra <daniel.int@outlook.com>
2023-07-08 20:24:46 -0500

    Update tagRouter
    - To get searchQuery, page, and limt
    
    - hanlde error with 500 http status

--- api/src/routes/tagRouter.js
+++ api/src/routes/tagRouter.js
@@ -1,13 +1,15 @@
-const tagRouter = require('express').Router();
-const { getTags } = require('../controllers/tagControllers');
+const tagRouter = require('express').Router()
+const { getTags } = require('../controllers/tagControllers')
 
-tagRouter.get('/', async (req, res) =>{
+tagRouter.get('/', async (req, res) => {
+    const { limit, page, searchQuery } = req.query
+
     try {
-        const tags = await getTags();
+        const tags = await getTags({ limit, page, searchQuery })
 
-        res.status(200).json(tags);
+        res.status(200).json(tags)
     } catch (error) {
-        res.status(404).send(error.message)
+        res.status(500).json({ error: error.message })
     }
 })
 
