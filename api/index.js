b66f608be2834568b3ef876da9f9e3b7a4f0097b
DanielRicra <daniel.int@outlook.com>
2023-07-08 19:58:50 -0500

    Update index.js
    Comente la parte donde se carga los datos a la base de datos, si quiere cargar datos descomentan los uploads, and force true

--- api/index.js
+++ api/index.js
@@ -1,18 +1,17 @@
 const server = require('./src/app.js')
-const { conn, Videogame, Tag, Genre } = require('./src/db.js')
-const {
-  uploadVideogames,
-  uploadTags,
-  uploadGenres,
-} = require('./src/utils/helpers.js')
+const { conn } = require('./src/db.js')
+// const {
+//   uploadVideogames,
+//   uploadTags,
+//   uploadGenres,
+// } = require('./src/utils/helpers.js')
 
 const PORT = 3001
 
-// Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
-  uploadGenres(Genre)
-  uploadTags(Tag)
-  uploadVideogames(Videogame)
+conn.sync({ force: false }).then(() => {
+  // uploadGenres(Genre)
+  // uploadTags(Tag)
+  // uploadVideogames(Videogame)
 
   server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`)
