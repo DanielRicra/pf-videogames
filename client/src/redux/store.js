b5c47a97e3264b25ecbf2d5a45f4994e9c00c7f1
DanielRicra <daniel.int@outlook.com>
2023-07-08 23:41:13 -0500

    Fix the import of review reducer in store.js

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -2,7 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
 import cartReducer from './cart/cartSlice'
 import videoGameReducer from './videogame/videoGameSlice'
-import reviewReducer from './Reducers/reviewReducer';
+import reviewReducer from './review/reviewReducer'
 
 const store = configureStore({
   reducer: {
