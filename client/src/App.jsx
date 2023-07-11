2c91aa2765db0dd303b646cc366813560ab7fdae
DanielRicra <daniel.int@outlook.com>
2023-07-10 19:45:18 -0500

    Wrap the app routes with layout component
    - Wrap the routes in a Route that have the Layout element, all the routes inside take renders inside a NavBar and Footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
 import { Create, Detail, Home, NotFound, Search } from './pages'
-import { NavBar } from './components'
+import { Layout } from './components'
 
 function App() {
   return (
-    <div className={'bg-[url(./assets/bg-img.jpg)] h-screen w-screen overflow-hidden'}>
+    <div
+      className={
+        'bg-[url(./assets/bg-img.jpg)] h-screen w-screen overflow-hidden'
+      }
+    >
       <div className='overflow-y-auto w-full h-full text-white'>
-        <NavBar />
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/search' element={<Search />} />
-          <Route path='/detail/:id' element={<Detail />} />
-          <Route path='/create' element={<Create />} />
+          <Route element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/search' element={<Search />} />
+            <Route path='/detail/:id' element={<Detail />} />
+            <Route path='/create' element={<Create />} />
+          </Route>
           <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
