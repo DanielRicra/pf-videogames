8e8212b38a7ff830c44c9e31793bc126036f99a3
DanielRicra <daniel.int@outlook.com>
2023-07-11 18:02:51 -0500

    Move the list of items to a CartItems.jsx

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -1,43 +1,20 @@
-import { IconTrashFilled } from '@tabler/icons-react'
-import { useDispatch, useSelector } from 'react-redux'
-import { getCartItems, removeFromCart } from '../redux/cart/cartSlice'
-import { formatMoney } from '../utils/helpers'
+import { useSelector } from 'react-redux'
+import { getCartItems } from '../../redux/cart/cartSlice'
+import { formatMoney } from '../../utils/helpers'
+import CartItems from './CartItems'
 
 const Cart = () => {
   const cartItems = useSelector(getCartItems)
-  const dispatch = useDispatch()
-
-  const handleRemoveItem = (id) => {
-    dispatch(removeFromCart(id))
-  }
 
   return (
-    <div className='min-h-screen flex flex-col justify-start items-start p-4 md:px-6 lg:px-10 xl:px-14'>
-      <div className='w-full bg-gray-100 text-black rounded-lg p-4'>
-        <h2 className='text-2xl font-semibold my-3'>Cart Shopping</h2>
-        {cartItems.map((item) => (
-          <div key={item.id} className='flex items-center my-3 justify-between'>
-            <div>
-              <div className='flex items-center w-[160px] aspect-[4/3]'>
-                <img
-                  src={item.image}
-                  alt={item.name}
-                  className='w-full h-full object-cover'
-                />
-              </div>
-              <p>{item.name}</p>
-            </div>
-            <div className='flex items-end gap-2 flex-col '>
-              <button onClick={() => handleRemoveItem(item.id)}>
-                <IconTrashFilled className='text-red-600' />
-              </button>
-              <p className='font-semibold text-purple-800'>{item.price} $</p>
-            </div>
-          </div>
-        ))}
+    <div className='min-h-screen flex flex-col justify-start items-start p-4 md:px-6 lg:px-10 xl:px-14 mb-10'>
+      <div className='w-full bg-gray-200 text-gray-800 rounded-lg p-6 px-10'>
+        <h2 className='text-2xl font-semibold my-3 border-b-2 border-gray-400'>Cart Shopping</h2>
+
+        <CartItems cartItems={cartItems} />
 
         <div className='w-full flex flex-col'>
-          <div className='flex items-center justify-between w-full my-3 border-b-2 border-purple-600'>
+          <div className='flex items-center justify-between w-full my-3 border-b-2 border-gray-400'>
             <p>Total VideoGames: {cartItems.length}</p>
             <p className='text-2xl flex gap-2'>
               <p>Total Price:</p>
