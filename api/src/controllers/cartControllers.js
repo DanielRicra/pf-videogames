13f2d1ed85cdac740d87994dea6908270c9fbda7
Francaios <donnarif@outlook.com>
2023-07-11 23:33:43 -0300

    Cree una ruta para crear un carrito con un array de videogamesId

--- api/src/controllers/cartControllers.js
+++ api/src/controllers/cartControllers.js
@@ -20,7 +20,7 @@ const addToCart = async (req, res) => {
     }
 
     // Obtener el carrito del usuario o crear uno nuevo si no existe
-    let cart = await Cart.findOne({ where: { userId: user.id } });
+    let cart = await Cart.findOne({ where: { userId: user.id , status: true } });
 
     if (!cart) {
       cart = await Cart.create({ userId: user.id });
@@ -42,7 +42,7 @@ const removeFromCart = async (req, res) => {
 
     // Verificar si el usuario y el videojuego existen
     const user = await User.findOne({ where: { email: userEmail } });
-    const cart = await Cart.findOne({ where: { userId: user.id } });
+    const cart = await Cart.findOne({ where: { userId: user.id , status: true } });
     const videogame = await Videogame.findByPk(videogameId);
 
     if (!cart || !videogame) {
@@ -58,6 +58,30 @@ const removeFromCart = async (req, res) => {
     return res.status(500).json({ error: 'Error interno del servidor' });
   }
 };
+
+const associateCart = async (req,res) =>{
+  try{
+
+    const { userEmail, videogamesId } =req.body
+
+    const user = await User.findOne({ where: { email: userEmail } });
+    const cart = await Cart.findOne({ where: { userId: user.id, status: true } });
+
+    if (!cart) {
+      cart = await Cart.create({ userId: user.id });
+    }
+
+    videogamesId.map( async videogame =>{
+      await Cart.addVideogame(videogame)
+    })
+
+    return res.status(200).json({ message: 'Carrito asociado al user con email' + userEmail });
+
+  }catch(error){
+    console.error('Error al asociar el carrito')
+    return res.status(500).json({error: 'Error interno del servidor'})
+  }
+}
 
 module.exports = {
   addToCart,
--- api/src/routes/cartRouter.js
+++ api/src/routes/cartRouter.js
@@ -1,7 +1,21 @@
 const cartRouter = require('express').Router();
-const { removeGame, addGame } = require('../controllers/cartControllers');
+const { removeGame, addGame, associateCart } = require('../controllers/cartControllers');
 const express = require('express');
 const router = express.Router();
+
+cartRouter.post('/add', async (req, res) =>{
+    try{
+
+        const cart = await associateCart(req)
+
+        if(cart.error) throw new Error(game.error)
+
+        res.status(200).json(cart)
+
+    }catch(error){
+        res.status(404).send(error.message)
+    }
+})
 
 cartRouter.post('/add/:videogameId', async (req, res) =>{
     try{
