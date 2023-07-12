9c4254ace366048a54372fe190a2ae40d2e32e13
DanielRicra <daniel.int@outlook.com>
2023-07-12 16:20:23 -0500

    Fix some styles and add logout icon

--- client/src/components/LogoutButton.jsx
+++ client/src/components/LogoutButton.jsx
@@ -1,14 +1,18 @@
-import { useAuth0 } from "@auth0/auth0-react"
+import { useAuth0 } from '@auth0/auth0-react'
+import { IconLogout } from '@tabler/icons-react'
 
 const LogoutButton = () => {
+  const { logout } = useAuth0()
 
-    const { logout } = useAuth0()
-
-    return (
-        <>
-            <button className="flex w-[4rem] text-[0.8rem] font-semibold justify-center bg-purple-600 border-purple-700 hover:bg-purple-700 hover:border-purple-600 transition duration-200 ease-in border-[0.2rem] rounded-[0.5rem] mx-[88rem] my-[-5rem]" onClick={() => logout()}>Log out</button>
-        </>
-    )
+  return (
+    <button
+      className='flex text-base font-semibold bg-purple-600 border-purple-700 hover:bg-purple-700 hover:border-purple-600 transition duration-200 ease-in border-[1px] rounded-[0.5rem] px-4 py-2 gap-2'
+      onClick={() => logout()}
+    >
+      Log out
+      <IconLogout />
+    </button>
+  )
 }
 
 export default LogoutButton
