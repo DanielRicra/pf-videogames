2e6e15e1476d2d9e10de685492d258b21cd1031b
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-07-12 16:58:26 -0500

    feat:(Cart,PaymentController): shopping cart connection with stripe

--- client/src/redux/cart/cartSlice.js
+++ client/src/redux/cart/cartSlice.js
@@ -3,6 +3,8 @@ import { getCartItemsFromStorage } from '../../utils/localStorageHelper'
 
 const initialState = {
   cartItems: getCartItemsFromStorage(),
+  loadingCheckoutStatus: false,
+  urlCheckout: '',
 }
 
 const cartSlice = createSlice({
@@ -13,16 +15,29 @@ const cartSlice = createSlice({
       state.cartItems.push(action.payload)
     },
     removeFromCart: (state, action) => {
-      state.cartItems = state.cartItems.filter((item) => item.id !== action.payload)
+      state.cartItems = state.cartItems.filter(
+        (item) => item.id !== action.payload
+      )
     },
     cleanCart: (state) => {
       state.cartItems = []
+    },
+    setLoadingCheckoutStatus: (state, action) => {
+      state.loadingCheckoutStatus = action.payload
+    },
+    setUrlCheckout: (state, action) => {
+      state.urlCheckout = action.payload
     },
   },
 })
 
 export const getCartItems = (state) => state.cart.cartItems
 
-export const { addToCart, removeFromCart, cleanCart } =
-  cartSlice.actions
+export const {
+  addToCart,
+  removeFromCart,
+  cleanCart,
+  setLoadingCheckoutStatus,
+  setUrlCheckout,
+} = cartSlice.actions
 export default cartSlice.reducer
