05c39a65c48e3458c8714fd926503e19a95ba4ce
DanielRicra <daniel.int@outlook.com>
2023-07-12 08:53:45 -0500

    Fix the style, show card responsive

--- client/src/components/VideoGameCardList.jsx
+++ client/src/components/VideoGameCardList.jsx
@@ -1,8 +1,9 @@
 import VideoGameCard from './VideoGameCard'
+import './index.css'
 
 const VideoGameCardList = ({ videogames = [], message }) => {
   return (
-    <div className='flex flex-wrap gap-4'>
+    <div className='gap-4 videogame__card-list'>
       {videogames.length === 0 && (
         <div className='flex items-center justify-center h-[300px] w-full'>
           <p className='text-4xl text-center font-semibold'>
