2ab4be6677b35b2fed248c5162e694c35887c1f0
DanielRicra <daniel.int@outlook.com>
2023-07-12 16:24:19 -0500

    Refactor and fix redundant code in Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,6 @@ import { Link } from 'react-router-dom'
 import LoginButton from '../components/LoginButton'
 import LogoutButton from '../components/LogoutButton'
 import { useAuth0 } from '@auth0/auth0-react'
-
 import { Loading } from '../components'
 
 const Home = () => {
@@ -15,62 +14,32 @@ const Home = () => {
 
   return (
     <div className='text-white min-h-screen py-14'>
-      {
-        isAuthenticated ?
-          <>
-            <LogoutButton />
-            
-            <div className='font-medium mb-5 text-7xl whitespace-normal ml-12'>
-              <h1 className='mt-[10rem] mb-[3rem] ml-2 lg:ml-10'>
-                Welcome to <span className='text-purple-400'>the best</span> <br /> PC
-                videogames <br /> E-commerce
-              </h1>
-            </div>
+      {isAuthenticated && <LogoutButton />}
 
-            <br />
+      <div className='font-medium mb-5 text-7xl whitespace-normal ml-12 min-h-[calc(100vh-120px)] flex justify-start items-center'>
+        <h1 className='mt-4 mb-[3rem] ml-2 lg:ml-10'>
+          Welcome to <span className='text-purple-400'>the best</span> <br /> PC
+          videogames <br /> E-commerce
+        </h1>
+      </div>
 
-            <div className='mb-5 mx-12 flex justify-between items-center'>
-              <h3 className='ml-2 text-4xl lg:ml-10 font-normal'>Popular Games</h3>
-              <Link to='/search' className='text-base'>
-                See all the games &#8594;
-              </Link>
-            </div>
+      <div className='mb-5 mx-12 flex justify-between items-center'>
+        <h3 className='ml-2 text-4xl lg:ml-10 font-normal'>Popular Games</h3>
+        <Link to='/search' className='text-base'>
+          See all the games &#8594;
+        </Link>
+      </div>
 
-            <Carrousel videGames={videogames} />
-            <br />
-          </>
+      <Carrousel videGames={videogames} />
+      <br />
 
-          :
-
-          <>
-            <div className='font-medium mb-5 text-7xl whitespace-normal ml-12'>
-              <h1 className='my-10 ml-2 lg:ml-10'>
-                Welcome to <span className='text-purple-400'>the best</span> <br /> PC
-                videogames <br /> E-commerce
-              </h1>
-            </div>
-
-            <br />
-
-            <div className='mb-5 mx-12 flex justify-between items-center'>
-              <h3 className='ml-2 text-4xl lg:ml-10 font-normal'>Popular Games</h3>
-              <Link to='/search' className='text-base'>
-                See all the games &#8594;
-              </Link>
-            </div>
-
-            <Carrousel videGames={videogames} />
-            <br />
-
-            <div className="font-medium text-6xl mt-12 flex justify-center items-center">
-              <h2>Join the community</h2>
-            </div>
-
-            <LoginButton />
-          </>
-      }
+      {!isAuthenticated && (
+        <div className='font-medium flex-col text-6xl mt-12 flex justify-center items-center min-h-[calc(100vh-96px)]'>
+          <h2>Join the community</h2>
+          <LoginButton />
+        </div>
+      )}
     </div>
   )
 }
 export default Home
-
