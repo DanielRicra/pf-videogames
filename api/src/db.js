8ec7eda9a21b4520cf343139addeec251358e87d
Francaios <donnarif@outlook.com>
2023-07-13 09:32:21 -0300

    prueba deploy

--- api/src/db.js
+++ api/src/db.js
@@ -12,7 +12,7 @@ const {
 
 // sequelize + SQL
 postgresql://postgres:KlB51jsJ4hABfYrYeJ9i@containers-us-west-136.railway.app:8030/railway
-const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}}/${DB_NAME}`, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
