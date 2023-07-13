3fd5c79a49e486ec77c8c97eb31166782568eace
DanielRicra <daniel.int@outlook.com>
2023-07-13 13:38:05 -0500

    Add thunk middleware to fetch user cart

--- client/src/redux/cart/cartSlice.js
+++ client/src/redux/cart/cartSlice.js
@@ -1,17 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit'
 import { getCartItemsFromStorage } from '../../utils/localStorageHelper'
+import {
+  addVideogamesToUserCart,
+  getCartByUserEmail,
+} from '../../services/cartService'
+import { AxiosError } from 'axios'
 
 const initialState = {
   cartItems: getCartItemsFromStorage(),
   loadingCheckoutStatus: false,
   urlCheckout: '',
+  checkoutError: '',
 }
+
+export const fetchCartByUserEmail = createAsyncThunk(
+  'users/fetchByIdStatus',
+  async (userEmail, { rejectWithValue, getState }) => {
+    try {
+      await addVideogamesToUserCart({
+        userEmail,
+        videogameIds: getState().cart.cartItems.map((item) => item.id),
+      })
+      const data = await getCartByUserEmail(userEmail)
+      return data
+    } catch (error) {
+      if (error instanceof AxiosError) {
+        return rejectWithValue(
+          error.response?.data?.error ?? 'Something when wrong'
+        )
+      }
+      return rejectWithValue(error.message)
+    }
+  }
+)
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
   reducers: {
     addToCart: (state, action) => {
+      if (state.cartItems.find((item) => item.id === action.payload.id)) return
       state.cartItems.push(action.payload)
     },
     removeFromCart: (state, action) => {
@@ -28,6 +56,19 @@ const cartSlice = createSlice({
     setUrlCheckout: (state, action) => {
       state.urlCheckout = action.payload
     },
+    setCheckoutError: (state, action) => {
+      state.loadingCheckoutStatus = false
+      state.checkoutError = action.payload
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchCartByUserEmail.fulfilled, (state, action) => {
+      const data = action.payload.videogames ?? []
+      const filteredData = data.filter(
+        (item) => !state.cartItems.some((i) => i.id === item.id)
+      )
+      state.cartItems.push(...filteredData)
+    })
   },
 })
 
@@ -39,5 +80,6 @@ export const {
   cleanCart,
   setLoadingCheckoutStatus,
   setUrlCheckout,
+  setCheckoutError,
 } = cartSlice.actions
 export default cartSlice.reducer
