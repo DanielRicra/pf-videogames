b94a50b91bb0ea84210ef9e129200c9dd36c38a6
Francaios <donnarif@outlook.com>
2023-07-12 22:27:57 -0300

    Otro cambio en al hacer el sequelize para el deploy

--- api/src/db.js
+++ api/src/db.js
@@ -7,11 +7,12 @@ const path = require('path');
 
 // se traen las credenciales
 const {
-  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT
 } = process.env;
 
 // sequelize + SQL
-const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
+postgresql://postgres:KlB51jsJ4hABfYrYeJ9i@containers-us-west-136.railway.app:8030/railway
+const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}}/${DB_NAME}`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
