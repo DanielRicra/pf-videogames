6efb29d9cb77165de3ade9bd8a238088457103e8
DanielRicra <daniel.int@outlook.com>
2023-07-15 15:46:30 -0500

    export Chat in page/index.js

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,5 +4,6 @@ import Home from './Home'
 import Search from './Search'
 import NotFound from './NotFound'
 import Cart from './Cart/Cart'
+import Chat from './Chat/Chat'
 
-export { Create, Detail, Home, Search, NotFound, Cart }
+export { Create, Detail, Home, Search, NotFound, Cart, Chat }
