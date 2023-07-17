b8701cec544465d089b50a646a99f6a6cd26fd55
XeroWTF <eugedsr@gmail.com>
2023-07-17 13:08:00 -0300

    TotiltCase

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -4,3 +4,12 @@ export const saveUser = async (user) => {
    const response = await api.post('/user/postUser', user)
    return response.data
 }
+
+export const getUserByEmail = async (email) => {
+   try {
+     const response = await api.get(`/user?email=${email}`)
+     return response.data
+   } catch (error) {
+     throw new Error('Error al obtener el usuario por email')
+   }
+ }
