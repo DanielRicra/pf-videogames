c5aef109cbede5695bf5c8d0520b3e4fb509b0ce
DanielRicra <daniel.int@outlook.com>
2023-07-17 18:53:36 -0500

    Add create, update and delete functions

--- client/src/pages/Dashboard/dataProvider.js
+++ client/src/pages/Dashboard/dataProvider.js
@@ -8,7 +8,7 @@ export const dataProvider = {
   getList: (resource, params) => {
     const { page, perPage } = params.pagination
     const { field, order } = params.sort
-    
+
     const url = `${API_URL}/${resource}?sort=${field}&order=${order}&page=${page}`
 
     return httpClient(url).then(({ headers, json }) => ({
@@ -16,8 +16,28 @@ export const dataProvider = {
       total: 10,
     }))
   },
+
   getOne: (resource, params) =>
     httpClient(`${API_URL}/${resource}/${params.id}`).then(({ json }) => ({
       data: json,
     })),
+
+  create: (resource, params) =>
+    httpClient(`${API_URL}/${resource}`, {
+      method: 'POST',
+      body: JSON.stringify(params.data),
+    }).then(({ json }) => ({
+      data: { ...params.data, id: json.id },
+    })),
+
+  update: (resource, params) =>
+    httpClient(`${API_URL}/${resource}/${params.id}`, {
+      method: 'PUT',
+      body: JSON.stringify(params.data),
+    }).then(({ json }) => ({ data: json })),
+
+  delete: (resource, params) =>
+    httpClient(`${API_URL}/${resource}/${params.id}`, {
+      method: 'DELETE',
+    }).then(({ json }) => ({ data: json })),
 }
