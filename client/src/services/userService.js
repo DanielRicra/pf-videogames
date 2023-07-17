dccf3892754b6de20ed61f6745b6ce3d32572377
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-07-17 10:26:32 -0500

    feat: create user database

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,6 +1,6 @@
 import { api } from './api'
 
 export const saveUser = async (user) => {
-   const response = await api.post('/user/postUser', user)
-   return response.data
+  const response = await api.post('/user/postUser', user)
+  return response.data
 }
