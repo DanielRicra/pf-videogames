f6f2ee6a7f55db10f4ce4b7632bab3a61942289b
DanielRicra <daniel.int@outlook.com>
2023-07-17 23:17:53 -0500

    Add get many in Tags and Genre routes
    - This /:ids endpoint is to bring the data on the ids.
    -example: `ids = '1,2,3'`

--- api/src/routes/genreRouter.js
+++ api/src/routes/genreRouter.js
@@ -1,29 +1,42 @@
-const genreRouter = require('express').Router();
-const { getGenres, postGenres } = require('../controllers/genreControllers');
+const genreRouter = require('express').Router()
+const {
+  getGenres,
+  postGenres,
+  getManyGenres,
+} = require('../controllers/genreControllers')
 
-genreRouter.get('/', async (req, res) =>{
-    try {
-        console.log('llegue a genreRouter')
-        const genres = await getGenres();
+genreRouter.get('/', async (req, res) => {
+  try {
+    const genres = await getGenres()
 
-        res.status(200).json(genres);
-    } catch (error) {
-        res.status(404).send(error.message)
-    }
+    res.status(200).json(genres)
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
 })
 
-genreRouter.post('/', async (req, res) =>{
-    try{
-        
-        const genre = await postGenres(req.body)
+genreRouter.get('/:ids', async (req, res) => {
+  const ids = req.params.ids.split(',').map(Number)
 
-        if(genre.error) throw new Error(genre.error)
+  try {
+    const genres = await getManyGenres(ids)
 
-        res.status(200).json(genre)
-    }
-    catch(error){
-        res.status(404).send(error.message)
-    }
+    res.status(200).json(genres)
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
+})
+
+genreRouter.post('/', async (req, res) => {
+  try {
+    const genre = await postGenres(req.body)
+
+    if (genre.error) throw new Error(genre.error)
+
+    res.status(200).json(genre)
+  } catch (error) {
+    res.status(404).send(error.message)
+  }
 })
 
 module.exports = genreRouter
