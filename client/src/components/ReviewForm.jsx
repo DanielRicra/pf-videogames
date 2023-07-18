618953237cf42c9bc7bfd979490c3cbf508a5818
XeroWTF <eugedsr@gmail.com>
2023-07-18 17:08:29 -0300

    responsabilidad en Library para boton reviewForm

--- client/src/components/ReviewForm.jsx
+++ client/src/components/ReviewForm.jsx
@@ -4,13 +4,12 @@ import { postReview } from '../redux/actions/reviewAction'
 import { IconX } from '@tabler/icons-react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { getUser } from '../services/userService'
-import useSWRImmutable from 'swr/immutable' 
-import { getReviewByVideogameId } from '../services/reviewService' 
+import useSWRImmutable from 'swr/immutable'
+import { getReviewByVideogameId } from '../services/reviewService'
 
-const ReviewForm = ({ videogameId, closeForm }) => {
+const ReviewForm = ({ videogameId, closeForm, onReviewSubmitted }) => {
   const [score, setScore] = useState(1)
   const [text, setText] = useState('')
-  const [isFormOpen, setIsFormOpen] = useState(true) 
   const dispatch = useDispatch()
   const { isAuthenticated, user } = useAuth0()
   const [userId, setUserId] = useState(null)
@@ -32,20 +31,13 @@ const ReviewForm = ({ videogameId, closeForm }) => {
 
   const { data: reviews, error: reviewError } = useSWRImmutable(videogameId, getReviewByVideogameId)
 
-
-  useEffect(() => {
-    
-  }, [videogameId])
-
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
 
-    
     const userReview = reviews?.find((review) => review.userId === userId)
 
     if (userReview) {
-      
-      setIsFormOpen(false) 
+      setIsFormOpen(false)
     } else {
       const review = {
         videogameId,
@@ -54,13 +46,15 @@ const ReviewForm = ({ videogameId, closeForm }) => {
         userId,
       }
 
-      dispatch(postReview(review))
-
-      // Reset form fields
-      setScore(1)
-      setText('')
-
-      closeForm()
+      try {
+        await dispatch(postReview(review))
+        setScore(1)
+        setText('')
+        closeForm()
+        onReviewSubmitted(true) // notifica a la libreria que se ha hecho submit de una review 
+      } catch (error) {
+        console.log('Error submitting review:', error)
+      }
     }
   }
 
@@ -71,59 +65,44 @@ const ReviewForm = ({ videogameId, closeForm }) => {
   return (
     <div className='w-full bg-transparent py-8'>
       <div className='max-w-3xl mx-auto px-4 relative'>
-        {isFormOpen && ( // Mostrar el formulario solo si está abierto
-          <>
-            <button
-              className='absolute top-2 right-2 text-red-600 hover:text-red-700 border-red-600 border-2 rounded-lg p-2'
-              onClick={closeForm}
+        <button
+          className='absolute top-2 right-2 text-red-600 hover:text-red-700 border-red-600 border-2 rounded-lg p-2'
+          onClick={closeForm}
+        >
+          <IconX className='w-8 h-8' />
+        </button>
+        <h2 className='text-3xl font-semibold text-center mb-6'>Review</h2>
+        <form onSubmit={handleSubmit} className='bg-white p-6 rounded-lg shadow-md'>
+          <div className='flex items-center justify-center'>
+            <input
+              type='range'
+              min='1'
+              max='10'
+              value={score}
+              onChange={(e) => setScore(e.target.value)}
+              className='w-64'
+            />
+            <span
+              className={`text-${score === '10' ? '4xl' : '2xl'} font-semibold text-black ml-2`}
             >
-              <IconX className='w-8 h-8' />
-            </button>
-            <h2 className='text-3xl font-semibold text-center mb-6'>Review</h2>
-            <form onSubmit={handleSubmit} className='bg-white p-6 rounded-lg shadow-md'>
-              <div className='flex items-center justify-center'>
-                <input
-                  type='range'
-                  min='1'
-                  max='10'
-                  value={score}
-                  onChange={(e) => setScore(e.target.value)}
-                  className='w-64'
-                />
-                <span
-                  className={`text-${score === '10' ? '4xl' : '2xl'} font-semibold text-black ml-2`}
-                >
-                  {score}
-                </span>
-              </div>
-              <div className='mt-4'>
-                <textarea
-                  value={text}
-                  onChange={(e) => setText(e.target.value)}
-                  placeholder='Do you like this game?'
-                  className='w-full h-32 px-3 py-2 border border-gray-300 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-purple-500'
-                ></textarea>
-              </div>
-              <button
-                type='submit'
-                className='bg-purple-600 text-white px-4 py-2 mt-4 rounded-md hover:opacity-80'
-              >
-                Submit
-              </button>
-            </form>
-          </>
-        )}
-        {!isFormOpen && ( // Mostrar el mensaje solo si el formulario está cerrado
-          <div className='text-center'>
-            <h2 className='text-3xl font-semibold mb-6'>¡Ya tienes una review para este videojuego!</h2>
-            <button
-              className='bg-purple-600 text-white px-4 py-2 rounded-md hover:opacity-80'
-              onClick={() => setIsFormOpen(true)}
-            >
-              Volver
-            </button>
+              {score}
+            </span>
           </div>
-        )}
+          <div className='mt-4'>
+            <textarea
+              value={text}
+              onChange={(e) => setText(e.target.value)}
+              placeholder='Do you like this game?'
+              className='w-full h-32 px-3 py-2 border border-gray-300 rounded-md text-black focus:outline-none focus:ring-2 focus:ring-purple-500'
+            ></textarea>
+          </div>
+          <button
+            type='submit'
+            className='bg-purple-600 text-white px-4 py-2 mt-4 rounded-md hover:opacity-80'
+          >
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   )
--- client/src/pages/Library.jsx
+++ client/src/pages/Library.jsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { getUser } from '../services/userService'
 import ReviewForm from '../components/ReviewForm'
+import { getReviewByVideogameId } from '../services/reviewService'
 
 const Library = () => {
   const { isAuthenticated, user } = useAuth0()
   const [cartItems, setCartItems] = useState([])
-  const [selectedVideogame, setSelectedVideogame] = useState(null) // Estado para mantener el id del videojuego seleccionado
+  const [selectedVideogame, setSelectedVideogame] = useState(null)
+  const [reviews, setReviews] = useState([])
+  const [hasReview, setHasReview] = useState({})
 
   useEffect(() => {
     const createUser = async () => {
@@ -23,17 +26,40 @@ const Library = () => {
     createUser()
   }, [isAuthenticated, user])
 
-  // Función para mostrar el formulario de reseña del videojuego seleccionado
+  useEffect(() => {
+    const fetchReviews = async () => {
+      try {
+        const videogameIds = cartItems.map((item) => item.id)
+        const reviewsPromises = videogameIds.map((id) => getReviewByVideogameId(id))
+        const reviewsData = await Promise.all(reviewsPromises)
+        const allReviews = reviewsData.flat()
+        setReviews(allReviews)
+
+        const hasReviewMap = {}
+        allReviews.forEach((review) => {
+          hasReviewMap[review.videogameId] = true
+        })
+        setHasReview(hasReviewMap)
+      } catch (error) {
+        console.log('Error fetching reviews:', error)
+      }
+    }
+
+    fetchReviews()
+  }, [cartItems])
+
   const handleReviewClick = (videogameId) => {
     setSelectedVideogame(videogameId)
+  }
+
+  const handleReviewSubmitted = (videogameId, hasReviewValue) => {
+    setHasReview((prevHasReview) => ({ ...prevHasReview, [videogameId]: hasReviewValue }))
   }
 
   return (
     <div className='min-h-screen flex flex-col justify-start items-start p-4 md:px-6 lg:px-10 xl:px-14 mb-10'>
       <div className='w-full bg-gray-200 text-gray-800 rounded-lg p-6 px-10'>
-        <h2 className='text-2xl font-semibold my-3 border-b-2 border-gray-400'>
-          My Games
-        </h2>
+        <h2 className='text-2xl font-semibold my-3 border-b-2 border-gray-400'>My Games</h2>
 
         <div className='flex flex-col gap-2 px-4'>
           {cartItems.map((item) => (
@@ -43,27 +69,23 @@ const Library = () => {
             >
               <div className='flex items-start gap-2'>
                 <div className='flex items-center w-[80px] h-[102px] overflow-hidden'>
-                  <img
-                    src={item.image}
-                    alt={item.name}
-                    className='w-full h-full object-cover'
-                  />
+                  <img src={item.image} alt={item.name} className='w-full h-full object-cover' />
                 </div>
                 <p>{item.name}</p>
               </div>
 
-              {/* Renderizar el botón de reseña por cada videojuego */}
-              <button
-                onClick={() => handleReviewClick(item.id)}
-                className='border border-purple-600 py-2 px-4 rounded'
-              >
-                Leave a Review
-              </button>
+              {!hasReview[item.id] && (
+                <button
+                  onClick={() => handleReviewClick(item.id)}
+                  className='border border-purple-600 py-2 px-4 rounded'
+                >
+                  Leave a Review
+                </button>
+              )}
             </div>
           ))}
         </div>
 
-        
         {selectedVideogame && (
           <div className='fixed top-0 left-0 w-full h-full flex justify-center items-center bg-gray-800 bg-opacity-50'>
             <div className='bg-white p-4 rounded-lg'>
@@ -71,6 +93,9 @@ const Library = () => {
               <ReviewForm
                 videogameId={selectedVideogame}
                 closeForm={() => setSelectedVideogame(null)}
+                onReviewSubmitted={(hasReviewValue) =>
+                  handleReviewSubmitted(selectedVideogame, hasReviewValue)
+                }
               />
             </div>
           </div>
