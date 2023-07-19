35e624f56dfb90ffc90a60729384affd25150bf5
DanielRicra <daniel.int@outlook.com>
2023-07-19 11:24:59 -0500

    Add UserEdit and UserCreate

--- client/src/pages/Dashboard/components/User.jsx
+++ client/src/pages/Dashboard/components/User.jsx
@@ -1,4 +1,4 @@
-import { Datagrid, EmailField, List, TextField } from 'react-admin'
+import { Create, Datagrid, Edit, EmailField, List, SimpleForm, TextField, TextInput } from 'react-admin'
 
 export const UserList = () => (
   <List>
@@ -12,3 +12,27 @@ export const UserList = () => (
     </Datagrid>
   </List>
 )
+
+export const UserEdit = () => (
+  <Edit>
+    <SimpleForm>
+      <TextInput source="id" />
+      <TextInput source="name" />
+      <TextInput source="email" />
+      <TextInput source="nickname" />
+      <TextInput source="friends" />
+    </SimpleForm>
+  </Edit>
+)
+
+export const UserCreate = () => {
+  return (
+    <Create>
+      <SimpleForm>
+      <TextInput source="name" />
+      <TextInput source="email" />
+      <TextInput source="nickname" />
+    </SimpleForm>
+    </Create>
+  )
+}
