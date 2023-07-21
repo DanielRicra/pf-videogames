b88574813c39bb9873fb574934fed97908021b55
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-07-21 17:03:28 -0500

    fix: corretion messages

--- client/src/pages/Chat/Message.jsx
+++ client/src/pages/Chat/Message.jsx
@@ -2,7 +2,9 @@ import { formatDistance } from 'date-fns'
 
 const Message = ({ message, friendId }) => {
   return (
-    <div className={message?.senderId === friendId ? 'self-end' : 'self-start'}>
+    <div
+      className={message?.senderId === friendId ? 'self-start ' : 'self-end'}
+    >
       <p>{message?.message}</p>
       <span className='text-sm'>
         {formatDistance(new Date(), new Date(message?.createdAt))}
