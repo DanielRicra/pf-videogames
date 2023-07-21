08bbf81bef41125a03db68f983556018cb05e21b
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-07-21 13:01:42 -0500

    feat

--- client/src/pages/Chat/Message.jsx
+++ client/src/pages/Chat/Message.jsx
@@ -2,10 +2,10 @@ import { formatDistance } from 'date-fns'
 
 const Message = ({ message, friendId }) => {
   return (
-    <div className={message.senderId === friendId ? 'self-end' : 'self-start'}>
-      <p>{message.message}</p>
+    <div className={message?.senderId === friendId ? 'self-end' : 'self-start'}>
+      <p>{message?.message}</p>
       <span className='text-sm'>
-        {formatDistance(new Date(), new Date(message.createdAt))}
+        {formatDistance(new Date(), new Date(message?.createdAt))}
       </span>
     </div>
   )
--- client/src/services/chatSevice.js
+++ client/src/services/chatSevice.js
@@ -1,11 +1,13 @@
 import { api } from './api'
 
-export const findOrCreateChat = async (friedShipId) => {
-  const response = await api.get(`/chat?friedShipId=${friedShipId}`)
+export const findOrCreateChat = async (friendShipId) => {
+  const response = await api.post(`/chat?friendShipId=${friendShipId}`)
   return response.data
 }
 
-export const addMessageToChat = async (friedShipId) => {
-  const response = await api.post(`/chat?friedShipId=${friedShipId}`)
+export const addMessageToChat = async ({ message, friendShipId }) => {
+  const response = await api.post(`/chat?friendShipId=${friendShipId}`, {
+    message,
+  })
   return response.data
 }
