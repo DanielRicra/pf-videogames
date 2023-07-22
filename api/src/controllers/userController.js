686ec59467c8d8e3d881bcc877a2c146bee8572b
DanielRicra <daniel.int@outlook.com>
2023-07-21 22:03:19 -0500

    Fix the getUserById

--- api/src/controllers/userController.js
+++ api/src/controllers/userController.js
@@ -23,18 +23,17 @@ const getUsers = async () => {
   }
 }
 
-const getUserById = async (req, res) => {
+const getUserById = async (id) => {
   try {
-    const { id } = req.params;
-    const user = await User.findByPk(id, { attributes: ['id', 'email', 'nickname'] });
+    const user = await User.findByPk(id)
     if (!user) {
-      return res.status(404).json({ error: 'User not found' });
+      throw new Error('User not found')
     }
-    res.status(200).json(user);
+    return user
   } catch (error) {
-    res.status(500).json({ error: 'Internal server error' });
+    throw error
   }
-};
+}
 
 // Obtener un usuario por su email
 const getUserByEmail = async (email) => {
