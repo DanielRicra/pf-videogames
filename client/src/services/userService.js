53d04550d7adceb00ac2e866369a4388ee0de477
DanielRicra <daniel.int@outlook.com>
2023-07-21 21:23:12 -0500

    Add getUserById and updateUser services

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,11 +1,21 @@
 import { api } from './api'
 
 export const saveUser = async (user) => {
-   const response = await api.post('/user', user)
-   return response.data
+  const response = await api.post('/user', user)
+  return response.data
 }
 
 export const getUser = async (email) => {
   const response = await api.get(`/user/${email}`)
+  return response.data
+}
+
+export const getUserById = async (url) => {
+  const response = await api.get(url)
+  return response.data
+}
+
+export const updateUser = async ({ id, user }) => {
+  const response = await api.put(`/user/${id}`, user)
   return response.data
 }
