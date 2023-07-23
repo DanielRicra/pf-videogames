c13d42270a1becb6aa005b872bcf495a812b9c1c
DanielRicra <daniel.int@outlook.com>
2023-07-23 15:55:46 -0500

    Use the crash image from cloudinary

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,4 +1,3 @@
-import crash from '../assets/crash.png'
 import { Link } from 'react-router-dom'
 
 const NotFound = () => {
@@ -8,8 +7,11 @@ const NotFound = () => {
         <div className='text-[3rem] font-mono font-semibold my-[2rem]'>
           Oops! this page does not exist
         </div>
-        <img src={crash} className='h-[15rem] relative' />
-   
+        <img
+          src='https://res.cloudinary.com/dopxkawke/image/upload/v1690145483/assets/crash_qvquyn.png'
+          className='h-[15rem] relative'
+        />
+
         <Link
           to='/'
           className='flex text-[1.3rem] font-semibold justify-center border-[0.3rem] border-blue-700 bg-blue-600 hover:bg-blue-700 hover:border-blue-600 duration-200 rounded-[0.5rem] p-3 px-6 my-[1.5rem]'
