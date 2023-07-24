60e4b2570fe592e4f0544ccd6f4effd87b49d252
DanielRicra <daniel.int@outlook.com>
2023-07-24 10:29:22 -0500

    Format favoriteService

--- client/src/services/favoriteService.js
+++ client/src/services/favoriteService.js
@@ -1,22 +1,20 @@
-import { api } from "./api";
+import { api } from './api'
 
 const saveFavorite = async (email, videogameId) => {
-    const response = await api.post(`/user/favorites?email=${email}&videogameId=${videogameId}`)
-    return response.data
+  const response = await api.post(
+    `/user/favorites?email=${email}&videogameId=${videogameId}`
+  )
+  return response.data
 }
 
 const deleteFavorite = async (id) => {
-    const response = await api.delete(`/user/favorites/${id}`);
-    return response.data
+  const response = await api.delete(`/user/favorites/${id}`)
+  return response.data
 }
 
 const getUserFavorites = async (url) => {
-    const response = await api.get(url)
-    return response.data
+  const response = await api.get(url)
+  return response.data
 }
 
-export {
-    saveFavorite,
-    deleteFavorite,
-    getUserFavorites
-}
+export { saveFavorite, deleteFavorite, getUserFavorites }
