8d73c786794a5ee40142b993990b33af93c97390
DanielRicra <daniel.int@outlook.com>
2023-07-25 00:41:15 -0500

    Change get to fetch to use in custom hok.
    - Rename `getPendingFriendRequests` to `fetchPendingFriendRequests`
    Now receive a url argument
    
    - Format code

--- client/src/services/friendService.js
+++ client/src/services/friendService.js
@@ -1,28 +1,28 @@
 import { api } from './api'
 
-
-export const getPendingFriendRequests = async (userEmail) => {
-  try {
-    const encodedEmail = encodeURIComponent(userEmail)
-    const response = await api.get(`/friend/pending/${encodedEmail}`)
-    return response.data
-  } catch (error) {
-    throw new Error('Error fetching pending friend requests')
-  }
+export const fetchPendingFriendRequests = async (url) => {
+  const response = await api.get(url)
+  return response.data
 }
 
 export const addFriend = async (userEmail, friendEmail) => {
-  const response = await api.post(`/friend?userEmail=${userEmail}&friendEmail=${friendEmail}&action=add`)
+  const response = await api.post(
+    `/friend?userEmail=${userEmail}&friendEmail=${friendEmail}&action=add`
+  )
   return response.data
 }
 
 export const acceptFriend = async (userEmail, friendEmail) => {
-  const response = await api.post(`/friend?userEmail=${userEmail}&friendEmail=${friendEmail}&action=accept`)
+  const response = await api.post(
+    `/friend?userEmail=${userEmail}&friendEmail=${friendEmail}&action=accept`
+  )
   return response.data
 }
 
 export const rejectFriend = async (userEmail, friendEmail) => {
-  const response = await api.post(`/friend?userEmail=${userEmail}&friendEmail=${friendEmail}&action=reject`)
+  const response = await api.post(
+    `/friend?userEmail=${userEmail}&friendEmail=${friendEmail}&action=reject`
+  )
   return response.data
 }
 
