4a72bf8eca35a34d19caf5c07df5e36b78cc86b1
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-07-25 13:26:19 -0500

    feat: last message chat

--- client/src/pages/Chat/ChatSideBar.jsx
+++ client/src/pages/Chat/ChatSideBar.jsx
@@ -1,4 +1,6 @@
 import { IconPointFilled } from '@tabler/icons-react'
+import { useEffect, useState } from 'react'
+import { findOrCreateChat } from '../../services/chatSevice'
 
 const ChatSideBar = ({
   friends,
@@ -6,6 +8,24 @@ const ChatSideBar = ({
   selecteFriend,
   lastMessage,
 }) => {
+  const [messages, setMessages] = useState({})
+  const getChat = async (idFriend) => {
+    const messagesData = await findOrCreateChat(idFriend)
+    const lastMessage =
+      messagesData[0]?.message[messagesData[0]?.message.length - 1]
+    setMessages((prevMessages) => ({
+      ...prevMessages,
+      [idFriend]: lastMessage,
+    }))
+  }
+
+  useEffect(() => {
+    if (friends) {
+      friends.forEach((friend) => {
+        getChat(friend.id)
+      })
+    }
+  }, [friends])
   return (
     <div className='flex flex-col gap-3 '>
       <div className='p-4'>
@@ -18,43 +38,52 @@ const ChatSideBar = ({
           />
         </div>
       </div>
-      {friends?.map((friend) => (
-        <div
-          className='flex gap-2 items-start cursor-pointer p-2'
-          style={{
-            backgroundColor:
-              selecteFriend === Number(friend.friendId)
-                ? '#8d8099'
-                : 'transparent',
-          }}
-          key={friend.id}
-          onClick={() =>
-            handleJoinChat({
-              idFriend: friend.friendId,
-              idUser: friend.userId,
-              friendShipId: friend.id,
-            })
-          }
-        >
-          <img
-            src={friend.friendUser.picture}
-            alt={friend.friendUser.name}
-            className='w-8 h-8 rounded-full'
-          />
+      {friends?.map((friend) => {
+        const message = messages[friend.id]
+        console.log(
+          '🚀 ~ file: ChatSideBar.jsx:44 ~ {friends?.map ~ message:',
+          message
+        )
+        return (
+          <div
+            className='flex gap-2 items-start cursor-pointer p-2'
+            style={{
+              backgroundColor:
+                selecteFriend === Number(friend.friendId)
+                  ? '#8d8099'
+                  : 'transparent',
+            }}
+            key={friend.id}
+            onClick={() =>
+              handleJoinChat({
+                idFriend: friend.friendId,
+                idUser: friend.userId,
+                friendShipId: friend.id,
+              })
+            }
+          >
+            <img
+              src={friend.friendUser.picture}
+              alt={friend.friendUser.name}
+              className='w-8 h-8 rounded-full'
+            />
 
-          <div className='flex gap-0 flex-row justify-between w-full'>
-            <div className='flex flex-col gap-0 '>
-              <p className='text-[12px] font-bold'>{friend.friendUser.name}</p>
-              <p className='text-xs'>{lastMessage?.message}</p>
-            </div>
+            <div className='flex gap-0 flex-row justify-between w-full'>
+              <div className='flex flex-col gap-0 '>
+                <p className='text-[12px] font-bold'>
+                  {friend.friendUser.name}
+                </p>
+                <p className='text-xs'>{message?.message}</p>
+              </div>
 
-            <div className='flex flex-col justify-between'>
-              <IconPointFilled size={'15px'} style={{ color: 'green' }} />
-              <p className='text-xs'> 2h</p>
+              <div className='flex flex-col justify-between'>
+                <IconPointFilled size={'15px'} style={{ color: 'green' }} />
+                <p className='text-xs'> 2h</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        )
+      })}
     </div>
   )
 }
