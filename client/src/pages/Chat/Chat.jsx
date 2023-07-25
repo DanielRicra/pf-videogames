36d0438634c759f140861e577ee1fc2bcfd1dbc0
DanielRicra <daniel.int@outlook.com>
2023-07-25 08:57:05 -0500

    Fix the fetch friend to do it inside the useEffect

--- client/src/pages/Chat/Chat.jsx
+++ client/src/pages/Chat/Chat.jsx
@@ -11,7 +11,6 @@ import { addMessageToChat, findOrCreateChat } from '../../services/chatSevice'
 
 import { toast } from 'sonner'
 
-
 const API_URL = import.meta.env.VITE_API_URL
 const socket = io(API_URL)
 
@@ -40,15 +39,15 @@ const Chat = () => {
   }, [messages])
 
   useEffect(() => {
-    if (isAuthenticated) {
-      Friends()
+    const fetchFriends = async () => {
+      const { results } = await getFriends(user.email)
+      setFriends(results)
     }
-  }, [user])
 
-  const Friends = async () => {
-    const { results } = await getFriends(user.email)
-    setFriends(results)
-  }
+    if (isAuthenticated) {
+      fetchFriends()
+    }
+  }, [isAuthenticated, user])
 
   const handleJoinChat = async ({ idUser, idFriend, friendShipId }) => {
     socket.emit('join', idUser)
