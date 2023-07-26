457bbf8d78e4686b798dba426e4c85f74c3247de
Ovelisco96 <123895378+Ovelisco96@users.noreply.github.com>
2023-07-26 14:39:02 -0500

    fix: correction payment

--- client/src/services/paymentService.js
+++ client/src/services/paymentService.js
@@ -1,6 +1,6 @@
 import { api } from './api'
 
 export const postPayment = async (data) => {
-   const response = await api.post('/payment/create-checkout-session', data)
-   return response.data
+  const response = await api.post('/payment/create-checkout-session', data)
+  return response.data
 }
